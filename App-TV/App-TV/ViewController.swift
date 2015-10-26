4b9cab38580339ee94d9be20d1005d9f2480af47
Pavel Jacko <pavelj@syerit.com>
2015-10-26 20:11:39 +0000

    added TV project

---
+++ App-TV/App-TV/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  App-TV
+//
+//  Created by Pavel Jacko on 26/10/2015.
+//  Copyright © 2015 Syerit. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
