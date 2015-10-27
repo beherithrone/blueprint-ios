ffc92d995e0a7911f0a1d38ba66e38e6b4a44e2c
Pavel Jacko <pavelj@syerit.com>
2015-10-27 18:40:30 +0000

    add fabric

--- App-iOS/Code/Source/Swift/AppDelegate.swift
+++ App-iOS/Code/Source/Swift/AppDelegate.swift
@@ -8,6 +8,8 @@
 import UIKit
 import Fabric
 import Crashlytics
+import TwitterKit
+import MoPub
 
 @UIApplicationMain
 class AppDelegate: UIResponder, UIApplicationDelegate {
@@ -17,7 +19,8 @@ class AppDelegate: UIResponder, UIApplicationDelegate {
 
     func application(application: UIApplication, didFinishLaunchingWithOptions launchOptions: [NSObject: AnyObject]?) -> Bool {
         // Override point for customization after application launch.
-        Fabric.with([Crashlytics.self])
+        Fabric.with([Crashlytics.self, Twitter.self, MoPub.self])
+
         return true
     }
 
